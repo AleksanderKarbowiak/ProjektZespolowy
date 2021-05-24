3bb7ed3c86e108f017aba7c834e7f5eef2686a74
AleksanderKarbowiak <63404639+AleksanderKarbowiak@users.noreply.github.com>
2021-05-24 17:39:46 +0200

    Front chlopakow

---
+++ SGGW_FORUM/script.js
@@ -0,0 +1,24 @@
+
+window.addEventListener('wheel', function(event)
+                        {
+    if (event.deltaY < 0)
+    {
+        document.getElementById('navbar').style.top="0px";
+    }
+    else if (event.deltaY > 0)
+    {
+        document.getElementById('navbar').style.top="-75px";
+    }
+});
+function show_menu()
+{
+    document.getElementById('navbar').style.top="0px";
+}
+function op_zamknij() {
+    document.getElementById("opinie").style.visibility = "hidden";
+    document.getElementById("opinie").style.opacity = "0";
+}
+function op_otworz() {
+    document.getElementById("opinie").style.visibility = "visible";
+    document.getElementById("opinie").style.opacity = "1";
+}
